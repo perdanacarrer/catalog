af1bdd1b6a4867cf3d639e925dc2beb4fbd53dbe
oscarperdana <sariminapp@gmail.com>
2024-05-28 09:08:48 +0700

    Initial Commit

---
+++ catalog/catalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  catalogApp.swift
+//  catalog
+//
+//  Created by oscar perdana on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct catalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
